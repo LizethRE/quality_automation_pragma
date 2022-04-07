2b5775e00b9e9135e70db997537721af3af70473
LizethRE2 <lizeth.rios@pragma.com.co>
2022-04-07 16:46:49 -0500

    Se cambio el escenario a corre a @login

--- src/test/java/co/com/pragma/certificacion/runners/EscenarioRunner.java
+++ src/test/java/co/com/pragma/certificacion/runners/EscenarioRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
 		features = "src/test/resources/features/",
-		tags = "@registro2",
+		tags = "@login",
 		glue = "co.com.pragma.certificacion.stepdefinitions",
 		snippets = CucumberOptions.SnippetType.CAMELCASE
 )
